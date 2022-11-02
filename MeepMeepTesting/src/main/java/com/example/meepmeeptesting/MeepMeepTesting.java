a9c357f865d7769365edc817530e8e265507aa8a
PWSH Robotics <mangevarun2006@gmail.com>
2022-11-02 18:23:27 -0500

    added meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,15 +14,13 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 61, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-35, 12, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-23.5, 12, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-23.5, 10, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-23.5, 12, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-35, 12, Math.toRadians(270)))
+                                .lineToLinearHeading(new Pose2d(-35, 30, Math.toRadians(270)))
                                 .build()
                 );
 
