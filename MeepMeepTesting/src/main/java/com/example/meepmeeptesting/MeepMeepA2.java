77065315fc084576112cfc543fb968de3f746404
nn2wg <nn2wg2@gmail.com>
2023-02-17 09:58:43 -0600

    tuned the PID controller

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepA2.java
@@ -0,0 +1,33 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepA2 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15.37)
+                .followTrajectorySequence(drive ->
+                    drive.trajectorySequenceBuilder(new Pose2d(34, -63, Math.toRadians(90)))
+                            .splineToConstantHeading(new Vector2d(12, -55), Math.toRadians(90))
+                            .lineToConstantHeading(new Vector2d(12, -42))
+                            .lineToSplineHeading(new Pose2d(12, -30, Math.toRadians(180)))
+                            .splineToConstantHeading(new Vector2d(5, -26), Math.toRadians(180))
+                            .splineToConstantHeading(new Vector2d(12, -18), Math.toRadians(90))
+                            .splineToSplineHeading(new Pose2d(12, -12, Math.toRadians(270)), Math.toRadians(0))
+                            .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunner/drive/DriveConstants.java
@@ -33,9 +33,9 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60.0 * TICKS_PER_REV));
+    public static final boolean RUN_USING_ENCODER = true;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(20, 0, 4,
+            2.7);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -46,8 +46,8 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8898; // in
-    public static double GEAR_RATIO = 1.01843184; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14.05; // in
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 21.39; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.016;
-    public static double kA = 0.0022;
-    public static double kStatic = 0.03;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0;
+    public static double kStatic = 0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -70,39 +70,27 @@ public class DriveConstants {
      * Note from LearnRoadRunner.com:
      * The velocity and acceleration constraints were calculated based on the following equation:
      * ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.85
-     * Resulting in 1007.6720463994614 in/s.
+     * Resulting in 52.48291908330528 in/s.
      * This is only 85% of the theoretical maximum velocity of the bot, following the recommendation above.
      * This is capped at 85% because there are a number of variables that will prevent your bot from actually
      * reaching this maximum velocity: voltage dropping over the game, bot weight, general mechanical inefficiencies, etc.
      * However, you can push this higher yourself if you'd like. Perhaps raise it to 90-95% of the theoretically
-     * max velocity. The theoretically maximum velocity is 1185.496525175837 in/s.
+     * max velocity. The theoretically maximum velocity is 61.74461068624151 in/s.
      * Just make sure that your bot can actually reach this maximum velocity. Path following will be detrimentally
      * affected if it is aiming for a velocity not actually possible.
      *
      * The maximum acceleration is somewhat arbitrary and it is recommended that you tweak this yourself based on
      * actual testing. Just set it at a reasonable value and keep increasing until your path following starts
-     * to degrade. As of now, it simply mirrors the velocity, resulting in 1007.6720463994614 in/s/s
+     * to degrade. As of now, it simply mirrors the velocity, resulting in 52.48291908330528 in/s/s
      *
      * Maximum Angular Velocity is calculated as: maximum velocity / trackWidth * (180 / Math.PI) but capped at 360°/s.
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
-     *
-     * WARNING: LearnRoadRunner.com's constant generator has capped the calculated recommended velocity at 90in/s.
-     * This message is showing because your gear ratio/motor RPM/etc. configuration, results in a recommended
-     * velocity (85% of max velocity) exceeding 90in/s.
-     * (Your recommended velocity was 1007.6720463994614in/s)
-     * This is simply insanely fast for an FTC bot and chances are your bot cannot properly reach these speeds.
-     *
-     * Just to be safe, LearnRoadRunner.com has arbitrarily limited your velocity to 90in/s.
-     * You are free to increase it yourself. If you do run into issues, please lower the maximum velocity.
-     *
-     * A documented case of a similar error which served as an impetus for this reasoning can be found here:
-     * https://github.com/acmerobotics/road-runner-quickstart/issues/91
 
      */
-    public static double MAX_VEL = 52.48291908330528;
-    public static double MAX_ACCEL = 52.48291908330528;
-    public static double MAX_ANG_VEL = Math.toRadians(181.14757590361444);
-    public static double MAX_ANG_ACCEL = Math.toRadians(181.14757590361444);
+    public static double MAX_VEL = 38;
+    public static double MAX_ACCEL = 38;
+    public static double MAX_ANG_VEL = Math.toRadians(231.31152000000003);
+    public static double MAX_ANG_ACCEL = Math.toRadians(231.31152000000003);
 
 
     public static double encoderTicksToInches(double ticks) {
